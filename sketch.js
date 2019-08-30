9063d2f305ecb90c668f94d7d847ed40a40c2bc7
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2019-08-31 04:06:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
---
+++ style.css
@@ -0,0 +1,7 @@
+html, body {
+  margin: 0;
+  padding: 0;
+}
+canvas{
+  border: 2px solid black;
+}
